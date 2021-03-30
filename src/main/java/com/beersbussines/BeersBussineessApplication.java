85f43591ad02b18b9df53a72de27ac206b8b1d69
Yoanni <ynfrometa@gmail.com>
2021-03-30 12:44:41 +0200

    Primer Commit

---
+++ src/main/java/com/beersbussines/BeersBussineessApplication.java
@@ -0,0 +1,13 @@
+package com.beersbussines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeersBussineessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeersBussineessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beersbussines/BeersBussineessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beersbussines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeersBussineessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
